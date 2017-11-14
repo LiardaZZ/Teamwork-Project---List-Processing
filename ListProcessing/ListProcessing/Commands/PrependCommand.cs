04af85a7b909b3c2bc7d2335058ad6114cc81ade
LiardaZZ <sashko_cv@abv.bg>
2017-11-14 23:09:08 +0200

    Added command functionality to "prepend" class.

--- ListProcessing/ListProcessing/Commands/PrependCommand.cs
+++ ListProcessing/ListProcessing/Commands/PrependCommand.cs
@@ -8,6 +8,17 @@
     {
         public List<string> Execute(List<string> input, IList<string> commandArgs)
         {
+            if (commandArgs.Count != 2)
+            {
+                Console.WriteLine("Error: invalid command parameters");
+                return input;
+            }
+
+            //add the string to the beginning of the input
+            var stringToPrepend = commandArgs[1];
+            input.Insert(0, stringToPrepend);
+
+            Console.WriteLine(String.Join(" ", input));
             return input;
         }
     }
