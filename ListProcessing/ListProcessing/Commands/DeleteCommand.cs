d873768ab86a0239240b3d45312154468cef0e5c
LiardaZZ <sashko_cv@abv.bg>
2017-11-14 23:26:19 +0200

    Added command functionality to "delete" class.

--- ListProcessing/ListProcessing/Commands/DeleteCommand.cs
+++ ListProcessing/ListProcessing/Commands/DeleteCommand.cs
@@ -8,6 +8,23 @@
     {
         public List<string> Execute(List<string> input, IList<string> commandArgs)
         {
+            int index;
+            var isInteger = int.TryParse(commandArgs[1], out index);
+
+            if (commandArgs.Count != 2 || !isInteger)
+            {
+                Console.WriteLine("Error: invalid command parameters");
+                return input;
+            }
+            else if (index < 0 || index > input.Count - 1)
+            {
+                Console.WriteLine($"Error: invalid index {index}");
+                return input;
+            }
+
+            input.RemoveAt(index);
+
+            Console.WriteLine(String.Join(" ", input));
             return input;
         }
     }
