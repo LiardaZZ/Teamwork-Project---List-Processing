fa7bfca4fa64ec62dcdee24eff39b43c46dacaff
LiardaZZ <sashko_cv@abv.bg>
2017-11-18 18:16:31 +0200

    Changed the IF statement in the StartUp class for the "end" command so the program can end normally.

--- ListProcessing/ListProcessing/StartUp.cs
+++ ListProcessing/ListProcessing/StartUp.cs
@@ -27,14 +27,13 @@
                 {
                    input = command.Execute(input, commandArgs);
                 }
-                commandArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
-
+                
                 if (commandArgs[0] == "end" && commandArgs.Count == 1)
                 {
-                    command = factory.CreateCommand(commandArgs);
-                    command.Execute(input, commandArgs);
                     break;
                 }
+
+                commandArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
             }
         }
     }
